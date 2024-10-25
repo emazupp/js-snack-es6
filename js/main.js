f1c854b4824975ff534bcd7f377d8ed5ec49bab7
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-25 15:57:46 +0200

    rimossi commenti

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
 
-/* const biciclette = [
+const biciclette = [
   {
     nome: "Velox",
     peso: {
@@ -50,7 +50,7 @@ const checkLighter = () => {
   return biciclette[indexLigter];
 };
 
-console.log(checkLighter()); */
+console.log(checkLighter());
 // ! Snack 2
 /* Creare un array di oggetti di squadre di calcio.
  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
@@ -58,7 +58,7 @@ Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
-/* const squadreDiCalcio = [
+const squadreDiCalcio = [
   {
     nome: "Milan",
     puntiFatti: 0,
@@ -103,7 +103,7 @@ for (let i = 0; i < squadreDiCalcio.length; i++) {
   nuovoArray[i] = squadreDiCalcio[i];
   delete nuovoArray[i].puntiFatti;
 }
-console.log(nuovoArray); */
+console.log(nuovoArray);
 
 // ! Snack 3 (Bonus)
 /* Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
